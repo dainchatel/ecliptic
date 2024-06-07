0cb7bad225f771f5cc9d4bafae862e5a39c85e8e
dainchatel <dain.chatel@gmail.com>
2024-06-07 17:15:29 -0400

    see why secret is not populating

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { app } from './server'
 
 const port = process.env.PORT ?? '3043'
-
+console.log('ugh', process.env)
 app.listen(port, () => {
   console.log(`We're up! Listening on port ${port}`)
 })
