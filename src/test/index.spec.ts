00e691ae8a93885ab385a2f90df001846b891fbe
dainchatel <dain.chatel@gmail.com>
2024-06-07 19:03:08 -0400

    use google-spreadsheets

--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -1,13 +1,13 @@
 import { app } from '../server'
 import request from 'supertest'
 
-describe('my block', () => {
-  it('does my stuff ', async () => {
+describe('endpoint testing', () => {
+  it('loads document at home route', async () => {
     const res = await request(app)
       .get('/')
       .expect(200)
 
-    expect(res.body.secret).toStrictEqual('heheheheh')
+    expect(res.body.docName).toStrictEqual('hotelsdotcom')
 
   })
 })
